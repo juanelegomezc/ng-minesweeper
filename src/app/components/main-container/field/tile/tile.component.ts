85834c10aec47ca22a9d02b49db80c69ad3f83fd
juanelegomezc <yo@juanelegomez.dev>
2023-07-31 11:55:53 -0500

    Show wrong blocked cells

--- src/app/components/main-container/field/tile/tile.component.ts
+++ src/app/components/main-container/field/tile/tile.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MinesweeperTile } from 'src/app/models/minesweeper-tile.model';
+import { MinesweeperService } from 'src/app/services/minesweeper.service';
 
 @Component({
     selector: 'app-tile',
@@ -9,4 +10,42 @@ import { MinesweeperTile } from 'src/app/models/minesweeper-tile.model';
 export class TileComponent {
     @Input()
     tile: MinesweeperTile = new MinesweeperTile();
+
+    constructor(private _minesweeperService: MinesweeperService) { }
+
+    showWrongOverlay(): boolean {
+        if (this._minesweeperService.isGameOver() && !this._minesweeperService.isWon()) {
+            if (!this.tile.isToggled()) {
+                if (this.tile.isBlocked()) {
+                    return !this.tile.isMined();
+                }
+            }
+        }
+        return false;
+    }
+
+    isButton(): boolean {
+        if (this._minesweeperService.isGameOver() && !this._minesweeperService.isWon()) {
+            if (!this.tile.isToggled()) {
+                if (this.tile.isBlocked()) {
+                    return this.tile.isMined();
+                } else {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return !this.tile.isToggled()
+        }
+    }
+
+    getValue(): string {
+        if (this._minesweeperService.isGameOver() && !this._minesweeperService.isWon()) {
+            if (this.tile.isBlocked() && !this.tile.isMined()) {
+                return "💣";
+            }
+        }
+        return this.tile.value;
+    }
 }
